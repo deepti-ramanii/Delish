65c8287f3ce2c88491245e9474ba1ea532dc5995
Deepti Ramani <deepti_ramani@hotmail.com>
2020-10-17 21:22:27 -0700

    rudimentary app working
    added basic functionality for create new food and log new meal

--- app/src/main/java/com/example/delish/CreateNewFood.java
+++ app/src/main/java/com/example/delish/CreateNewFood.java
@@ -4,12 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class CreateNewFood extends Fragment {
+    EditText getFoodNameInput;
+    String foodName;
+
+    EditText getNumServingsInput;
+    int numServings;
+
+    Button getNumIngredientsButton;
+    EditText getNumIngredientsInput;
+    int numIngredients;
+
+    Button submitFoodButton;
+    List<EditText> ingredientNameInputs = new ArrayList<EditText>();
+    Set<String> ingredients = new HashSet<String>();
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +41,50 @@ public class CreateNewFood extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_create_new_food, container, false);
+
+        //get name of new food
+        getFoodNameInput = (EditText)view.findViewById((R.id.get_food_name));
+
+        //get number of servings in new food
+        getNumServingsInput = (EditText)view.findViewById((R.id.get_num_servings));
+
+        //get ingredients of new food
+        getNumIngredientsInput = (EditText)view.findViewById(R.id.get_num_ingredients);
+        getNumIngredientsButton = (Button)view.findViewById(R.id.get_num_ingredients_button);
+        getNumIngredientsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numIngredients = Integer.parseInt(getNumIngredientsInput.getText().toString());
+                /*
+                TODO: get changing number of ingredients working
+                for(int i = 0; i < numIngredients; i++) {
+                    //add corresponding number of input fields
+                }
+                */
+                //add the ingredients to a set
+                ingredientNameInputs.add((EditText)view.findViewById(R.id.ingredient_input_1));
+                ingredientNameInputs.add((EditText)view.findViewById(R.id.ingredient_input_2));
+                ingredientNameInputs.add((EditText)view.findViewById(R.id.ingredient_input_3));
+                ingredientNameInputs.add((EditText)view.findViewById(R.id.ingredient_input_4));
+                ingredientNameInputs.add((EditText)view.findViewById(R.id.ingredient_input_5));
+            }
+        });
+
+        submitFoodButton = (Button)view.findViewById(R.id.submit_food_button);
+        submitFoodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                foodName = getFoodNameInput.getText().toString().toLowerCase();
+                numServings = Integer.parseInt(getNumServingsInput.getText().toString());
+                for(int i = 0; i < numIngredients; i++) {
+                    ingredients.add(ingredientNameInputs.get(i).getText().toString().toLowerCase());
+                }
+            }
+        });
+
+        Food newFood = new Food(foodName, numServings, ingredients);
+        //store the new food in the directory somewhere
+
         return view;
     }
 }
--- app/src/main/java/com/example/delish/LogNewMeal.java
+++ app/src/main/java/com/example/delish/LogNewMeal.java
@@ -1,15 +1,32 @@
 package com.example.delish;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LogNewMeal extends Fragment {
+    Button getNumFoodsButton;
+    EditText getNumFoodsInput;
+    int numFoods = 0;
+
+    Button submitMealButton;
+    List<EditText> foodNameInputs = new ArrayList<EditText>();
+    List<String> foodNames = new ArrayList<String>();
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +36,43 @@ public class LogNewMeal extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_log_new_meal, container, false);
+
+        getNumFoodsButton = (Button)view.findViewById(R.id.get_num_foods_button);
+        getNumFoodsInput = (EditText)view.findViewById(R.id.get_num_foods);
+
+        getNumFoodsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numFoods = Integer.parseInt(getNumFoodsInput.getText().toString());
+                /*
+                TODO: get variable number of ingredients working
+                EditText temp;
+                for(int i = 0; i < numFoods; i++) {
+                    temp = view.findViewById(R.id.food_name_input_1);
+                    temp.setId(i);
+                    temp.setHint("Enter food item " + (i + 1));
+                    layout = (LinearLayout)getActivity().findViewById(R.id.logNewMeal);
+                    layout.addView(temp);
+                }
+                */
+                foodNameInputs.add((EditText)view.findViewById(R.id.food_name_input_1));
+                foodNameInputs.add((EditText)view.findViewById(R.id.food_name_input_2));
+                foodNameInputs.add((EditText)view.findViewById(R.id.food_name_input_3));
+                foodNameInputs.add((EditText)view.findViewById(R.id.food_name_input_4));
+                foodNameInputs.add((EditText)view.findViewById(R.id.food_name_input_5));
+            }
+        });
+        
+        submitMealButton = (Button) view.findViewById(R.id.submit_meal_button);
+        submitMealButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < numFoods; i++) {
+                    foodNames.add(foodNameInputs.get(i).getText().toString().toLowerCase());
+                }
+            }
+        });
+
         return view;
     }
 }
